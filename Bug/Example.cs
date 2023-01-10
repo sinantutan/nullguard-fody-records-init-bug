4cf80ba74091fffd843de08ae44c2196899f9de7
Sinan Tutan <sinantutancoding@gmail.com>
2023-01-10 15:38:57 +0100

    add example

---
+++ Bug/Example.cs
@@ -0,0 +1,31 @@
+﻿namespace Bug;
+
+public record FooRecordWithInit
+{
+    private int _id;
+
+    public int Id
+    {
+        init => _id = value; //< ERROR: Common Language Runtime detected an invalid program.
+    }
+}
+
+public record FooRecordWithSet
+{
+    private int _id;
+
+    public int Id
+    {
+        set => _id = value; //< Works
+    }
+}
+
+public class FooClassWithInit
+{
+    private int _id;
+
+    public int Id
+    {
+        init => _id = value; //< Works
+    }
+}
